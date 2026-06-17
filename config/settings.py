1a6c68bf72bf2f2061ac2aa0ecd8211c66386aa4
saadsh15 <saadsh15@users.noreply.github.com>
2026-06-17 04:50:10 -0600

    Fix file upload 500 error on Vercel by routing MEDIA_ROOT to /tmp

--- config/settings.py
+++ config/settings.py
@@ -107,7 +107,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+if 'VERCEL' in os.environ or not DEBUG:
+    MEDIA_ROOT = '/tmp/media'
+else:
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
